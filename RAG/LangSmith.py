6f67590180d24af010509f9f6384301d77a8fdca
Sukmadi <54486758+MathematicGuy@users.noreply.github.com>
2025-07-04 17:38:31 +0700

    finally figure out JSON output format

---
+++ RAG/LangSmith.py
@@ -0,0 +1,8 @@
+# with open('langsmith_api_key', 'r') as f:
+#     langsmith_key = f.read()
+
+# LANGSMITH_TRACING=true
+# LANGSMITH_ENDPOINT="https://api.smith.langchain.com"
+# LANGSMITH_API_KEY=langsmith_key
+# LANGSMITH_PROJECT="rag-aio-project"
+# OPENAI_API_KEY="<your-openai-api-key>"
---
+++ RAG/RAG.py
@@ -0,0 +1,204 @@
+import os
+import time
+import torch
+from pathlib import Path
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    BitsAndBytesConfig,
+    pipeline,
+)
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_huggingface.embeddings import HuggingFaceEmbeddings
+from langchain_experimental.text_splitter import SemanticChunker
+from langchain_chroma import Chroma
+from langchain_huggingface.llms import HuggingFacePipeline
+from langchain import hub
+from langchain_core.output_parsers import StrOutputParser, JsonOutputParser
+from langchain_core.runnables import RunnableParallel, RunnablePassthrough, RunnableLambda, RunnableMap
+from langchain_core.prompts import PromptTemplate
+
+def get_device():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Using device: {device}")
+    return device
+
+
+def load_embeddings():
+    return HuggingFaceEmbeddings(model_name="bkai-foundation-models/vietnamese-bi-encoder")
+
+
+def load_llm(model_name):
+    token_path = Path("token.txt")
+    if not token_path.exists():
+        raise FileNotFoundError("Missing HuggingFace token.txt")
+
+    with token_path.open("r") as f:
+        hf_token = f.read().strip()
+
+    bnb_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_use_double_quant=True,
+        bnb_4bit_compute_dtype=torch.bfloat16,
+        bnb_4bit_quant_type="nf4"
+    )
+
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name,
+        quantization_config=bnb_config,
+        low_cpu_mem_usage=True,
+        device_map="auto",
+        token=hf_token
+    )
+
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    tokenizer.pad_token = tokenizer.eos_token
+
+    model_pipeline = pipeline(
+        "text-generation",
+        model=model,
+        tokenizer=tokenizer,
+        max_new_tokens=512,
+        pad_token_id=tokenizer.eos_token_id,
+        device_map="auto"
+    )
+
+    return HuggingFacePipeline(pipeline=model_pipeline)
+
+
+def load_documents(folder_path):
+    folder = Path(folder_path.strip().strip('"\''))
+
+    if not folder.exists():
+        raise FileNotFoundError(f"Folder not found: {folder}")
+
+    pdf_files = list(folder.glob("*.pdf"))
+    if not pdf_files:
+        raise ValueError(f"No PDF files in folder: {folder}")
+
+    all_docs, filenames = [], []
+    for pdf_file in pdf_files:
+        try:
+            loader = PyPDFLoader(str(pdf_file))
+            docs = loader.load()
+            all_docs.extend(docs)
+            filenames.append(pdf_file.name)
+            print(f"✅ Loaded {pdf_file.name} ({len(docs)} pages)")
+        except Exception as e:
+            print(f"❌ Failed loading {pdf_file.name}: {e}")
+    return all_docs, filenames
+
+
+def build_rag_chain(docs, embeddings, llm):
+    chunker = SemanticChunker(
+        embeddings=embeddings,
+        buffer_size=1,
+        breakpoint_threshold_type="percentile",
+        breakpoint_threshold_amount=95,
+        min_chunk_size=500,
+        add_start_index=True
+    )
+
+    chunks = chunker.split_documents(docs)
+    vector_db = Chroma.from_documents(chunks, embedding=embeddings)
+    retriever = vector_db.as_retriever()
+    prompt_template =  PromptTemplate.from_template("""
+        You are an assistant for question-answering tasks.
+        Use the following pieces of retrieved context to answer the question.
+        If you don't know the answer, just say that you don't know.
+
+        Return your response as a valid JSON object with exactly three keys: "context", "question", and "answer".
+
+        - The "context" key should contain the retrieved context.
+        - The "question" key should contain the user's question.
+        - The "answer" key should contain your answer to the question.
+
+        Only output the JSON object, nothing else.
+
+        {{
+            "context": "{context}",
+            "question": "{question}",
+        }}
+
+        "response":
+        """
+    )
+    #? next test: for loop 10 times to check if output
+
+    def format_docs(docs):
+        return "\n\n".join(doc.page_content for doc in docs)
+
+    # rag_chain = (
+    #     #? { } -> Syntax for RunnableMap.
+    #     #? retriever is the retrieved information. format_docs is function to format the retrieved information.
+    #     RunnableParallel({'context': retriever | format_docs, 'question': RunnablePassthrough()})
+    #     | prompt
+    #     | llm
+    #     | StrOutputParser()
+    # )
+
+    # return_json_result = RunnableLambda(lambda result: {
+    #     "context": result["context"],
+    #     "question": result["question"],
+    #     "answer": result
+    # })
+
+    rag_chain = (
+        RunnableMap({
+            'context': retriever | format_docs,
+            'question': RunnablePassthrough(),
+        })
+        | prompt_template
+        | llm
+        | StrOutputParser()
+    )
+
+    return rag_chain, len(chunks)
+
+
+def main():
+    get_device()
+    embeddings = load_embeddings()
+    #? PhoGPT-5.5B
+    #? Phi-2 (2.7B)
+    #? lmsys/vicuna-7b-v1.5
+    MODEL_NAME= "google/gemma-2b-it"
+    # MODEL_NAME = "lmsys/vicuna-7b-v1.5"
+    llm = load_llm(MODEL_NAME)
+
+    folder_path = "pdf_folder"  # Replace with your path
+    start = time.time()
+    docs, filenames = load_documents(folder_path)
+    rag_chain, num_chunks = build_rag_chain(docs, embeddings, llm)
+
+    print(f"\nReady: {len(filenames)} files, {num_chunks} chunks")
+    print(f"⏱️ Loading Time: {time.time() - start:.2f}s")
+
+    # while True:
+        # user_input = input("\nYour question (type 'exit' to quit): ")
+        # if user_input.lower() == "exit":
+        #     break
+    print("\nGenerating answer...")
+
+    start = time.time()
+    response = rag_chain.invoke("OOP là gì ?")
+
+    # try:
+    print(f"\nContext: {response['response']['context']}")
+    print('------------------------------------')
+    print(f"Questionf: {response['response']['question']}")
+    print('------------------------------------')
+    print(f"JSON OUTPUT: {response['response']['answer']}")
+
+    with open("output.txt", "a", encoding="utf-8") as f:
+        f.write(str(response) + "\n")
+
+    # except Exception as error:
+    #     print(f"Error: {error}")
+    #     print(f"JSON OUTPUT: {response}")
+
+    print(f"⏱️ Time taken: {time.time() - start:.2f}s")
+
+
+if __name__ == "__main__":
+    main()
